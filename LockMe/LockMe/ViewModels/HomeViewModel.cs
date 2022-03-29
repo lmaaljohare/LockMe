bb898948ee4d65f3c09b71e066c8611efd30da5d
ayamyadmeh <ayamyadmeh@gmail.com>
2022-03-29 14:43:38 +0300

    commint

--- LockMe/LockMe/ViewModels/HomeViewModel.cs
+++ LockMe/LockMe/ViewModels/HomeViewModel.cs
@@ -1,10 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using LockMe.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Text;
+using Xamarin.Forms;
+using Android.Graphics.Drawables;
+using System.Collections.Generic;
 
 namespace LockMe.ViewModels
 {
-    internal class HomeViewModel
+
+    public class HomeViewModel : INotifyPropertyChanged
     {
+        private ObservableCollection<HomePageModel> installedApps;
+        private bool isChecked;
+        public ObservableCollection<HomePageModel> InstalledApps
+        {
+            get { return installedApps; }
+            set
+            {
+
+                installedApps = value;
+            }
+        }
+
+        public HomeViewModel()
+        {
+            List<HomePageModel> listOfInstalledApps = DependencyService.Get<IAndroidService>().GetIntalledApps();
+            InstalledApps = new ObservableCollection<HomePageModel>(listOfInstalledApps);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+
+
     }
 }
